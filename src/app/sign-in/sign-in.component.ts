6b14057384fd18c47f937775fd9234f59f6f3468
Venkatesh Kadimisetty <venkatesh.kadimisetty@quantela.com>
2018-05-23 12:01:10 +0530

    basic login form and dashboard

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,27 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() {
-  }
-
   username : string
   password : string
 
-  login(event) : void {
-    console.log("event",event);
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+  }
+
+  login(inputForm) : void {
     if(this.username == 'admin' && this.password == 'admin'){
-     //this.router.navigate(["user"]);
-     alert("login Successfull");
+      console.log("form data",inputForm);
+     alert("login successfully!!")
+     this.router.navigate(['/dashboard']);
     }else {
       alert("Invalid credentials");
     }
   }
-  ngOnInit() {
-  }
-
 }
