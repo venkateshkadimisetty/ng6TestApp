45096648c34f882aa0730919b8a9bc6b1fc50453
Venkatesh Kadimisetty <venkatesh.kadimisetty@quantela.com>
2018-05-23 12:09:29 +0530

    basic login with dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import {NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent,
-    NavbarComponent
+    SignInComponent
   ],
   imports: [
     BrowserModule,
